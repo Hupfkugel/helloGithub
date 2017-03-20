b5a390efef53f4d9309d9c6f2c9493634d9670bc
Julia <LIU17203@spengergasse.at>
2017-03-20 22:12:28 +0100

    second commit

---
+++ Product.java
@@ -0,0 +1,19 @@
+public class Product
+{
+	private String name = "Coca Cola";
+	
+	public Product (String name)
+	{
+		setName(name);
+	}
+	
+	public getName()
+	{
+		return name;
+	}
+	
+	public setName(String name)
+	{
+		this.name= name;
+	}
+}
